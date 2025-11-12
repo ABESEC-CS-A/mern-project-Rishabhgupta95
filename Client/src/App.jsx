c1f0ba5b1dc3397daac307e07e3ddd6b28704470
Rishabh Gupta <rishabhgupta@Rishabhs-MacBoo.local>
2025-11-12 18:41:50 +0530

    route

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,23 +1,29 @@
 import React, { useState } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './App.css';
-import Home from './Component/Home';
 import Header from './Component/Header';
-import About from './Component/about';
 import Help from './Component/Help';
+import Home from './Component/Home';
+import About from './Component/about';
+import ViewCart from './Component/ViewCart';
+import Vieworder from './Component/Vieworder';
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <BrowserRouter>
-      <Header />
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/about" element={<About />} />
-        <Route path="/help" element={<Help />} />
-      </Routes>
-    </BrowserRouter>
+     <div>
+       <BrowserRouter>
+         <Header/>
+          <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path='/about' element={<About/>} />
+            <Route path='/viewcart' element={<ViewCart/>} />
+            <Route path='/vieworder' element={<Vieworder/>} />
+            <Route path='/help' element={<Help />} />
+          </Routes>
+       </BrowserRouter>
+    </div>
   );
 }
 
--- Client/src/Component/Header.jsx
+++ Client/src/Component/Header.jsx
@@ -1,12 +1,16 @@
-import React from 'react';
+import {nav} from 'react';
 import { Link } from 'react-router-dom';
 
 const Header = () => {
   return (
-    <div style={{ display: 'flex', gap: '1rem', padding: '1rem', background: '#f3f3f3' }}>
-      <Link to="/">Home</Link>
-      <Link to="/about">About</Link>
-      <Link to="/help">Help</Link>
+    <div>
+        <nav style={{ display: 'flex', gap: '3rem', padding: '1rem',backgroundColor:'red'}}>
+           <Link to="/">Home</Link>
+           <Link to="/about">About</Link>
+           <Link to="/help">Help</Link>
+           <Link to="/viewcart">ViewCart</Link>
+           <Link to="/vieworder">ViewOrder</Link>
+        </nav>
     </div>
   );
 };
